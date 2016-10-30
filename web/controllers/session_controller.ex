08a8345805d5debf9f88472fc16e0292d20b3cae
David Thompson <dtt101@gmail.com>
2016-10-30 20:23:56 +0000

    Add auth and login

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,21 @@
+defmodule Rumbl.SessionController do
+  use Rumbl.Web, :controller
+
+  def new(conn, _) do
+    render conn, "new.html"
+  end
+
+  def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
+    case Rumbl.Auth.login_by_username_and_password(conn, user, pass, repo: Rumbl.Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome")
+        |> redirect to: page_path(conn, :index)
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Invalid details")
+        |> render("new.html")
+    end
+  end
+
+end
