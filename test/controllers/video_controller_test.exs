0b4a80c86502ce1f7178addd9718c58cb615e48b
David Thompson <dtt101@gmail.com>
2016-11-11 17:51:05 +0000

    Add tests for video controller

---
+++ test/controllers/video_controller_test.exs
@@ -0,0 +1,50 @@
+defmodule Rumbl.VideoControllerTest do
+  use Rumbl.ConnCase
+
+  alias Rumbl.Video
+
+  @valid_attrs %{url: "http://www.example.com/video", title: "Test", description: "Test video"}
+
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "max"
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video  = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
+
+  @tag login_as: "max"
+  test "creates users video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+      get(conn, video_path(conn, :new)),
+      get(conn, video_path(conn, :index)),
+      get(conn, video_path(conn, :show, "123")),
+      get(conn, video_path(conn, :edit, "123")),
+      put(conn, video_path(conn, :update, "123", %{})),
+      post(conn, video_path(conn, :create, %{})),
+      delete(conn, video_path(conn, :delete, "123")),
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
+  end
+end
